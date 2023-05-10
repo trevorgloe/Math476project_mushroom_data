d1531cec9a405e4f1d6819b23c32f0ab0748d71b
Trevor <trevorgloe@gmail.com>
2023-05-10 15:17:19 -0700

    added street data for SLO county and added cocode to plot it

--- read_data.py
+++ read_data.py
@@ -3,6 +3,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import descartes
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
 
 data = pd.read_csv('observations-319470.csv')
 print(data)
@@ -15,4 +18,9 @@ print(lon)
 
 fig = plt.figure()
 plt.scatter(lat,lon,s=1.0)
+
+street_map = gpd.read_file('Roads_-_All.shp')
+fig, ax = plt.subplots(figsize=(15,15))
+street_map.plot(ax=ax)
+
 plt.show()
