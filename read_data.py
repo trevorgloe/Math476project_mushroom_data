00dbfe96bf0b2311d88d7b70624ab7750f980a5c
Trevor <trevorgloe@gmail.com>
2023-05-17 16:00:08 -0700

    added code to make species array and such

--- read_data.py
+++ read_data.py
@@ -13,7 +13,7 @@ temp = os.path.dirname(curpath)
 datapath = os.path.join(temp,'mushroom_proj_data')
 
 data = pd.read_csv('observations-319470.csv')
-print(data)
+#print(data)
 
 lat = data['latitude'].to_numpy()
 lon = data['longitude'].to_numpy()
@@ -21,13 +21,31 @@ species = data['scientific_name'].to_numpy()
 
 # print(lat)
 # print(lon)
-print(species)
+#print(species)
 
 # cut off the lattiude and longitude 
 lat[lon<-140]='nan'
 lon[lon<-140]='nan'
 
-print(lat)
+# add colors corresponding to the species
+num_fungi = np.count_nonzero(species =='Fungi')
+
+print(len(np.unique(species)))
+print(len(species))
+print(num_fungi)
+
+uni_species = np.unique(species)
+# find the species with the highest occurances
+# create dataframe of species with their respective counts
+counts = np.zeros(uni_species.shape)
+for i in range(len(uni_species)):
+    counts[i] = np.count_nonzero(species == uni_species[i])
+
+species_counts = pd.DataFrame(np.vstack((uni_species,counts)).T,columns=['Species','Occurances'])
+# print(species_counts)
+print(species_counts.to_string())
+
+#print(lat)
 
 fig = plt.figure()
 plt.scatter(lat,lon,s=1.0)
@@ -43,5 +61,12 @@ river_map2 = gpd.read_file(os.path.join(datapath,'NHD_Major_Lakes_and_Reservoirs
 river_map2.plot(ax=ax,color='blue')
 
 plt.scatter(lon,lat,color='r',s=2.0)
+
+lat_fungi = np.copy(lat)
+lon_fungi = np.copy(lon)
+lat_fungi[species!='Fungi'] = 'nan'
+lon_fungi[species!='Fungi'] = 'nan'
+
+plt.scatter(lon_fungi,lat_fungi,color='green',s=2)
 
 plt.show()
