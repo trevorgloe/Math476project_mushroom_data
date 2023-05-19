0b5fa6dcd6ecc2091ef0ebf69ad32749504c7fae
Madison Lytle <madison@pop-os.localdomain>
2023-05-19 12:14:30 -0700

    ooga booga

---
+++ k_beans_bustering.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from numpy import linalg as LA
+from numpy.random import default_rng
+import scipy.sparse
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+#np.seterr(divide='ignore', invalid='ignore')
+rng = default_rng(12345)
+
+curpath = os.getcwd()
+temp = os.path.dirname(curpath)
+datapath = os.path.join(temp,'mushroom_proj_data')
+
+data = pd.read_csv('observations-319470.csv')
+
+lat_n = data['latitude'].to_numpy()
+lon_n = data['longitude'].to_numpy()
+lat = lat_n[np.logical_not(np.isnan(lat_n))] # remove nans
+lon = lon_n[np.logical_not(np.isnan(lon_n))]
+# lat[lon<-140]='nan'
+# lon[lon<-140]='nan'
+
+#latlon = data[['latitude','longitude']].to_numpy()
+latlon = np.vstack((lat, lon))
+print(latlon)
+#print(lat.shape)
+
+def opt_reps(X, k, assign):
+  (n, d) = X.shape
+  reps = np.zeros((k, d))
+  for i in range(k):
+    in_i = [j for j in range(n) if assign[j] == i]
+    reps[i,:]= np.sum(X[in_i,:],axis=0) / len(in_i)
+  return reps
+
+def opt_clust(X, k, reps):
+  (n, d) = X.shape
+  dist = np.zeros(n)
+  assign = np.zeros(n, dtype=int)
+  for j in range(n):
+    dist_to_i = np.array([LA.norm(X[j,:] - reps[i,:]) for i in range(k)])
+    assign[j] = np.argmin(dist_to_i)
+    dist[j] = dist_to_i[assign[j]]
+  G = np.sum(dist ** 2)
+  print(G)
+  return assign
+
+def mmids_kmeans(X, k, maxiter=10):
+  (n, d) = X.shape
+  assign = rng.integers(0,k,n)
+  reps = np.zeros((k, d), dtype=int)
+  for iter in range(maxiter):
+    reps = opt_reps(X, k, assign)
+    assign = opt_clust(X, k, reps)
+  return assign
+  
+assign = mmids_kmeans(latlon, 5)
+plt.scatter(latlon[:,0], latlon[:,1], c=assign)
+plt.show()
