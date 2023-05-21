3631606fe934cdbde9323ad68522cc9fd13280bb
Trevor Loe <trevorgloe@gmail.com>
2023-05-20 17:22:04 -0700

    initial commit

---
+++ remove_badpts.py
@@ -0,0 +1,5 @@
+### Script to remove all the points whos lat and lon put them outside of the california state boundaries
+#
+# Takes all the points, returns only the points who are within the california state boundaries
+
+import numpy as np
